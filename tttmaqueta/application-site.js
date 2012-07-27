6fb09b8c2825f3a69ac03cf69c0b12bbf46711de
”Fernando <ceafernando@gmail.com>
2012-07-27 01:32:40 -0300

    agragado animacion de los items del menu

--- tttmaqueta/application-site.js
+++ tttmaqueta/application-site.js
@@ -7,6 +7,21 @@
 		    menuBox.css('position', ($(this).scrollTop() > 130) ? 'fixed' : 'relative' );
 		});
 
+		$('.item')
+			.on('mouseover',function(){ $(this).animate({
+				opacity: 0.9,
+				marginTop: "-2px",
+				color: "#FF4E00",
+				borderBottomWidth: "2px"
+			}, "fast" ) })
+			.on('mouseout',function(){ $(this).animate({
+				opacity: 1,
+				marginTop: "0px",
+				color: "white",
+				borderBottomWidth: "0px"
+			}, "fast" ) });
+
+
 	})
 
 }(window.jQuery)
