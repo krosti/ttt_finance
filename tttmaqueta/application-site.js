0a16835f24f3a46a5508b6d08a02d251d903ff94
andreslh <andreslh90@gmail.com>
2012-07-27 01:47:02 -0300

    Objeto de Slider creado

--- tttmaqueta/application-site.js
+++ tttmaqueta/application-site.js
@@ -6,6 +6,8 @@
 		$(window).scroll(function(){
 		    menuBox.css('position', ($(this).scrollTop() > 130) ? 'fixed' : 'relative' );
 		});
+		slider.iniciar_slider($('#slideshow'), $('#slidesContainer'),$('.slide'),450,"");
+		slider.iniciar_slider($('#slideshow2'), $('#slidesContainer2'),$('.slide2'),450,"2");
 
 	})
 
